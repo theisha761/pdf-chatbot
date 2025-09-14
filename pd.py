b7aa5c027540b7a260d580610698fd5394ccf25a
disha <disha.shukla.ug24@nsut.ac.in>
2025-09-15 03:55:38 +0530

    ok

--- pd.py
+++ pd.py
@@ -1,12 +1,10 @@
-from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAIEmbeddings
+from langchain.llms import LlamaCpp
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 import streamlit as st
-from dotenv import load_dotenv
-load_dotenv()
 st.title("chat with your pdf 📝")
 uploaded_file=st.file_uploader("upload your pdf", type="pdf")
 if uploaded_file is not None:
@@ -16,10 +14,13 @@ if uploaded_file is not None:
         docs=loader.load()
         spilliter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=10)
         chunks=spilliter.split_documents(docs)
-        embed=OpenAIEmbeddings()
+        embed = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
         vector_store=Chroma.from_documents(chunks,embed)
-        model=ChatOpenAI()
-        chain=RetrievalQA.from_llm(model,retriever=vector_store.as_retriever(search_kwargs={"k":2}))
+        llm = LlamaCpp(
+        model_path="models/llama-2-7b-chat.ggmlv3.q4_0.bin", 
+        n_ctx=2048
+    )
+        chain=RetrievalQA.from_llm(llm,retriever=vector_store.as_retriever(search_kwargs={"k":2}))
         query=st.text_input("ask question related to pdf")
         if query:
             result=chain.run(query)
